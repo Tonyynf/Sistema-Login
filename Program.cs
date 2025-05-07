ca77f4bfaf14965366ae7e25af07ed067e81ddc6
Tonyynf <tobyencarnacao@gmail.com>
2025-05-06 22:51:23 -0300

    Adicionando o método verificarSenha()

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿public class Program{
+﻿using System.Net.Mail;
+
+public class Program{
     static void Main(string[] args){
         User user1 = new User("123joao@gmail.com", "ddd123","Anthony");
 
-        
+        string senhaSolicitada = Console.ReadLine();
+
+        user1.verificarSenha();
         Console.WriteLine(user1.nome + " " + user1.email);
     }
 }
--- User.cs
+++ User.cs
@@ -1,13 +1,27 @@
+using System.Diagnostics.Contracts;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
 
 public class User{
+    public string nome{get;set;}
     public string email{ get; set;}
     public string senha{get; private set;}
-    public string nome{get;set;}
+    
 
-    public User(string email, string senha, string nome){
+    public User(string nome, string email, string senha){
+        this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.nome = nome;
+    }
+
+    public void verificarSenha(){
+        string senhaSolicitada = "";
+
+        if(senhaSolicitada == senha){
+            Console.WriteLine("APERTE ENTER");
+        }else{
+            Console.WriteLine("Senha incorreta!");
+        }
     }
 }

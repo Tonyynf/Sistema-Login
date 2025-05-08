6e979df0df85a5377ee2eb7b45b76552bc82362c
Tonyynf <tobyencarnacao@gmail.com>
2025-05-08 19:29:03 -0300

    Alterações automáticas do pacote NuGet

--- Program.cs
+++ Program.cs
@@ -5,29 +5,9 @@ public class Program
     static void Main(string[] args)
     {
         User user1 = new User("123joao@gmail.com", "ddd123", "Anthony");
-        //teste
-        Console.WriteLine("Digite uma senha: ");
-        string senha1 = Console.ReadLine();
 
-        Console.WriteLine("Digite a mesma senha novamente senha: ");
-        string senha2 = Console.ReadLine();
-
-        string hash1 = HashHelper.GerarHash(senha1);
-
-        string hash2 = HashHelper.GerarHash(senha2);
-
-        Console.WriteLine($"\nHash 1: {hash1}");
-        Console.WriteLine($"Hash 2: {hash2}");
-
-        if (hash1 == hash2)
-        {
-            Console.WriteLine("Hashs iguais");
-        }
-        else
-        {
-            Console.WriteLine("Hashs diferentes");
-        }
-        //string senhaSolicitada = Console.ReadLine();
+        Console.WriteLine("Menu:\n1 - Cadastro\n2 - Login\n3 - Exclusão de conta\n4 - Alteração dados\n5 - Sair");
+        
         /* Colocar metodo para gerar hash*/
         Console.WriteLine(user1.nome + " " + user1.email);
     }
---
+++ UserRepository.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class UserRepository{
+    List<string> dataUsers = new List<string>(); 
+    /* Terminar de fazer o sitema de armazenamento de dados */ 
+
+}

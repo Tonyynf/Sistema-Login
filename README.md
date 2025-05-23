7e8007fa8ca2e7f31e732fdcc742ff2584bb908f
Tonyynf <tobyencarnacao@gmail.com>
2025-05-23 13:15:12 -0300

    Criando README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+✔️ Sistema de Login (CRUD)
+
+📝 Descrição
+
+Projeto com o intuito de criar um sistema de login no próprio console onde contem as funções de cadastro, mostrar usuários, excluir usuário, alterar usuário, login e sair(para encerrar a aplicação).
+
+🔧 Tecnologias utilizadas
+
+VS Code; C#;
+
+🎯 Status do projeto
+
+Finalizado.
--- obj/Debug/net8.0/SistemaLogin.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaLogin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de82b751e0606aa90a4275176fe6bca90506f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0230a2907a301f8d360db2aa4152445d47c94545")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
